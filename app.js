ad1674bd7ab937b866b5e633c514fdd4dfab659e
Timur <timur87@mail.ru>
2024-01-07 03:59:04 +0300

    refactor: update HTML and JS (add alternative content for multimedia) according to rule 1.2 in html-and-css-extended.md

--- app.js
+++ app.js
@@ -48,6 +48,7 @@ var createNewTaskElement=function(taskString){
   deleteButton.className="button delete";
   deleteButtonImg.className="delete-img";
   deleteButtonImg.src="./remove.svg";
+  deleteButtonImg.alt="remove";
   deleteButton.appendChild(deleteButtonImg);
 
 
